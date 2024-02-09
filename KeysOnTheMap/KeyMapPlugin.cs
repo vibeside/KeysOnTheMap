183ad7a4d403b52e252cf90a7929175d9b83b2c5
vibeside <vibeside01@gmail.com>
2024-02-09 12:15:26 -0500

    mod works as intended, key icon is square. will move to an image instead

--- KeysOnTheMap/KeyMapPlugin.cs
+++ KeysOnTheMap/KeyMapPlugin.cs
@@ -1,7 +1,9 @@
 ﻿using BepInEx;
+using KeysOnTheMap.Scripts;
 using MonoMod.RuntimeDetour;
 using System;
 using System.Reflection;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -20,8 +22,17 @@ namespace KeysOnTheMap
             {
                 if (self.itemProperties.itemName == "Key")
                 {
-
-                }
+                    GameObject KeyIcon = new GameObject();
+                    KeyIcon.transform.SetParent(self.transform, false);
+                    KeyIcon.transform.SetLocalPositionAndRotation(new Vector3(0, 0.5f, 0), Quaternion.Euler(new Vector3(90, 0, 0)));
+                    KeyIcon.transform.localScale = new Vector3(5f, 5f, 5f);
+                    KeyIcon.layer = StartOfRound.Instance.itemRadarIconPrefab.layer;
+                    KeyIcon.name = "keyicon :3";
+                    KeyIcon.AddComponent<KeepUpwards>();
+                    TextMeshPro tmpro = KeyIcon.AddComponent<TextMeshPro>();
+                    tmpro.alignment = TextAlignmentOptions.Center;
+                    tmpro.text = "🔑";
+                } 
                 //self.radarIcon = Instantiate(StartOfRound.Instance.itemRadarIconPrefab, RoundManager.Instance.mapPropsContainer.transform).transform;
                 original(self);
             });
