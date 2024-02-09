a454742428f3581d38bae396333546a2884ece7c
vibeside <vibeside01@gmail.com>
2024-02-09 11:50:27 -0500

    first commit

---
+++ KeysOnTheMap/KeyMapPlugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using MonoMod.RuntimeDetour;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace KeysOnTheMap
+{
+    [BepInPlugin(modGUID,modName,"9.28.0.5")]
+    public class KeyMapPlugin : BaseUnityPlugin
+    {
+        public const string modGUID = "grug.lethalcompany.KeysOnMap";
+        public const string modName = "Keys On Map";
+        public void Awake()
+        {
+            Hook KeyHook = new(
+            typeof(GrabbableObject).GetMethod(nameof(GrabbableObject.Start), (BindingFlags)int.MaxValue),
+            (Action<GrabbableObject> original, GrabbableObject self) =>
+            {
+                if (self.itemProperties.itemName == "Key")
+                {
+
+                }
+                //self.radarIcon = Instantiate(StartOfRound.Instance.itemRadarIconPrefab, RoundManager.Instance.mapPropsContainer.transform).transform;
+                original(self);
+            });
+        }
+
+    }
+}
---
+++ KeysOnTheMap/Scripts/KeepUpwards.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace KeysOnTheMap.Scripts
+{
+    public class KeepUpwards : MonoBehaviour
+    {
+        public void Update()
+        {
+            gameObject.transform.rotation = Quaternion.Euler(90, -45, 0);
+        }
+    }
+}
